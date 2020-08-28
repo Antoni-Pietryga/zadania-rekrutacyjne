d64fc60d94d35c26646661abd27fbc52420506f6
Antoni-Pietryga <64539405+Antoni-Pietryga@users.noreply.github.com>
2020-08-28 10:57:47 +0200

    Add files via upload

--- modeAnalyzer.py
+++ modeAnalyzer.py
@@ -3,26 +3,17 @@ class ModeAnalyzer:
         self.number_of_chars_tab = number_of_chars_tab
 
     def check_mode(self, lines):
-        tabs, spaces, spaces_in_line, tabs_in_line = 0, 0, 0, 0
+        tabs, spaces = 0, 0
 
         for line in lines:
-            spaces_in_line, tabs_in_line = 0, 0
-
             for char in line:
 
                 if char == " ":
-                    spaces_in_line += 1
+                    spaces += 1
                 elif char == "\t":
-                    tabs_in_line += 1
-                else:
-                    break
+                    tabs += 1
 
-            spaces_in_line //= self.number_of_chars_tab
-
-            if spaces_in_line > tabs_in_line:
-                spaces += 1
-            elif spaces_in_line < tabs_in_line:
-                tabs += 1
+                break
 
         if spaces > tabs:
             return "spaces"
